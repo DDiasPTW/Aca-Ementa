4c78411a0bfe18fba4cde6b3531830e4f59361dc
Dias <dmpldias@gmail.com>
2023-10-18 21:53:14 +0100

    2nd Update
    Added re ordering of the displayed items
    Added searching for certain items

--- Ementa_Aca/Assets/Scripts/DishManager.cs
+++ Ementa_Aca/Assets/Scripts/DishManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,33 +24,51 @@ public class DishManager : MonoBehaviour
     private bool isInEditMode = false;
     private List<Dish> dishes = new List<Dish>();
 
-
+    [Header("Saving")]
     private string dataPath;
+
+    [Header("Other")]
+    //ordering
+    public Button activeTitleButton;
+    public Button nameButton;
+    [SerializeField] private bool isSortedAscending = true;
+    [SerializeField] private bool isSortedAlphabetically = true;
+    //searching
+    public TMP_InputField searchInputField;
+    private Coroutine searchCoroutine;
+
 
     void Start()
     {
-        dataPath = Application.persistentDataPath + "/dishes.txt";
+        dataPath = Application.persistentDataPath + "/Pratos.txt";
         LoadDishesFromFile();
+        //editing and saving
         editButton.onClick.AddListener(ToggleEditMode);
         saveButton.onClick.AddListener(SaveChanges);
         saveButton.gameObject.SetActive(false);
+        //reordering
+        activeTitleButton.onClick.AddListener(SortByActiveStatus);
+        nameButton.onClick.AddListener(SortByAlpha);
+        //searching
+        searchInputField.onValueChanged.AddListener(delegate { StartDelayedSearch(); });
     }
 
+    #region Creating/Loading/Deleting
     public void ToggleAddDishPanel()
     {
         // This function toggles the visibility of the "Add Dish" panel
         addDishPanel.SetActive(!addDishPanel.activeSelf);
     }
-
     public void CreateDish()
     {
         Dish newDish = new Dish
         {
-        name = dishNameInput.text,
-        category = categoryDropdown.options[categoryDropdown.value].text,
-        priceHalfServing = float.Parse(halfDosePriceInput.text),
-        priceFullServing = float.Parse(fullDosePriceInput.text),
-        isSoldOut = false
+        nome = dishNameInput.text,
+        categoria = categoryDropdown.options[categoryDropdown.value].text,
+        precoMeia = float.Parse(halfDosePriceInput.text),
+        precoDose = float.Parse(fullDosePriceInput.text),
+        Esgotado = false,
+        isAtivo = false
         };
         dishes.Add(newDish);
 
@@ -60,15 +79,110 @@ public class DishManager : MonoBehaviour
         dishNameInput.text = "";
         categoryDropdown.value = 0;
         halfDosePriceInput.text = "";
-        fullDosePriceInput.text = "";;
+        fullDosePriceInput.text = "";
+    }
+    public void SaveDishesToFile()
+    {
+        string path = Application.persistentDataPath + "/Pratos.txt";
+        string json = JsonUtility.ToJson(new SerializableDishList { dishes = dishes }, true); // Convert the list of dishes to JSON
+        File.WriteAllText(path, json); // Write the JSON to the file
+    }
+    public void LoadDishesFromFile()
+    {
+        string path = Application.persistentDataPath + "/Pratos.txt";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SerializableDishList loadedDishes = JsonUtility.FromJson<SerializableDishList>(json);
+            dishes = loadedDishes.dishes;
+
+            foreach (Dish dish in loadedDishes.dishes)
+            {
+                // Instantiate the dish UI and set the properties
+                InstantiateDishUI(dish);
+            }
+        }
+    }
+    private void InstantiateDishUI(Dish dish)
+    {
+        // Create new dish instance in the UI
+        GameObject newDishUI = Instantiate(dishPrefab, dishesContainer);
+        dishUIObjects.Add(newDishUI);
+
+        // Set properties for the new dish instance
+        TMP_InputField nameField = newDishUI.transform.GetChild(1).GetComponent<TMP_InputField>();
+        TMP_Dropdown categoryDropdown = newDishUI.transform.GetChild(2).GetComponent<TMP_Dropdown>();
+        TMP_InputField halfDoseField = newDishUI.transform.GetChild(3).GetComponent<TMP_InputField>();
+        TMP_InputField fullDoseField = newDishUI.transform.GetChild(4).GetComponent<TMP_InputField>();
+        Toggle soldOutToggle = newDishUI.transform.GetChild(5).GetComponent<Toggle>();
+        Toggle ativoToggle = newDishUI.transform.GetChild(6).GetComponent<Toggle>();
+        Button deleteButton = newDishUI.transform.GetChild(7).GetComponent<Button>();
+
+        soldOutToggle.onValueChanged.AddListener((isOn) =>
+        {
+            dish.Esgotado = isOn;
+            SaveDishesToFile(); // Save immediately after change
+        });
+
+        ativoToggle.onValueChanged.AddListener((isOn) =>
+        {
+            dish.isAtivo = isOn;
+            SaveDishesToFile(); // Save immediately after change
+        });
+
+
+        // Populate the UI with the dish data
+        nameField.text = dish.nome;
+        categoryDropdown.value = categoryDropdown.options.FindIndex(option => option.text == dish.categoria); 
+        halfDoseField.text = dish.precoMeia.ToString();
+        fullDoseField.text = dish.precoDose.ToString();
+        soldOutToggle.isOn = dish.Esgotado;
+        ativoToggle.isOn = dish.isAtivo;
+
+        // Disable input for the created dish details (they will only be editable after hitting "Editar")
+        nameField.interactable = false;
+        categoryDropdown.interactable = false;
+        halfDoseField.interactable = false;
+        fullDoseField.interactable = false;
+
+        // Add delete button functionality
+        deleteButton.onClick.AddListener(() =>
+        {
+            DeleteDish(newDishUI);
+        });
+    }
+    public void DeleteDish(GameObject dish)
+    {
+        int indexToRemove = dishUIObjects.IndexOf(dish);
+
+        Toggle soldOutToggle = dish.transform.GetChild(5).GetComponent<Toggle>();
+        Toggle ativoToggle = dish.transform.GetChild(6).GetComponent<Toggle>();
+
+        // Remove listeners
+        soldOutToggle.onValueChanged.RemoveAllListeners();
+        ativoToggle.onValueChanged.RemoveAllListeners();
+
+        if (indexToRemove >= 0 && indexToRemove < dishes.Count)
+        {
+            dishes.RemoveAt(indexToRemove);
+            SaveDishesToFile();
+        }
+
+        dishUIObjects.Remove(dish);
+        //Debug.Log("Removing... " + dish.name);
+        Destroy(dish);
     }
 
+    #endregion
+
+    #region Edit
     private void ToggleEditMode()
     {
         isInEditMode = !isInEditMode; // Toggle edit mode
         saveButton.gameObject.SetActive(isInEditMode);
 
-        foreach(GameObject dish in dishUIObjects)
+        foreach (GameObject dish in dishUIObjects)
         {
             TMP_InputField nameField = dish.transform.GetChild(1).GetComponent<TMP_InputField>();
             TMP_Dropdown categoryDropdown = dish.transform.GetChild(2).GetComponent<TMP_Dropdown>();
@@ -94,16 +208,14 @@ public class DishManager : MonoBehaviour
             TMP_Dropdown categoryDropdown = dish.transform.GetChild(2).GetComponent<TMP_Dropdown>();
             TMP_InputField halfDoseField = dish.transform.GetChild(3).GetComponent<TMP_InputField>();
             TMP_InputField fullDoseField = dish.transform.GetChild(4).GetComponent<TMP_InputField>();
-            Toggle soldOutToggle = dish.transform.GetChild(5).GetComponent<Toggle>();
 
             // Update the dish list with the latest UI values
             Dish updatedDish = new Dish
             {
-                name = nameField.text,
-                category = categoryDropdown.options[categoryDropdown.value].text,
-                priceHalfServing = float.Parse(halfDoseField.text),
-                priceFullServing = float.Parse(fullDoseField.text),
-                isSoldOut = soldOutToggle.isOn
+                nome = nameField.text,
+                categoria = categoryDropdown.options[categoryDropdown.value].text,
+                precoMeia = float.Parse(halfDoseField.text),
+                precoDose = float.Parse(fullDoseField.text),
             };
             dishes.Add(updatedDish);
 
@@ -119,88 +231,138 @@ public class DishManager : MonoBehaviour
 
         saveButton.gameObject.SetActive(false);
     }
+    #endregion
 
-    public void SaveDishesToFile()
+    #region Other
+
+    private enum SortState
     {
-        string path = Application.persistentDataPath + "/dishes.txt";
-        string json = JsonUtility.ToJson(new SerializableDishList { dishes = dishes }, true); // Convert the list of dishes to JSON
-        File.WriteAllText(path, json); // Write the JSON to the file
+        Ascending,
+        Descending
+    }
+    private SortState currentSortState = SortState.Ascending;
+    public void SortByActiveStatus()
+    {
+        switch (currentSortState)
+        {
+            case SortState.Ascending:
+                dishes.Sort((a, b) => a.isAtivo.CompareTo(b.isAtivo));
+                currentSortState = SortState.Descending;
+                break;
+
+            case SortState.Descending:
+                dishes.Sort((a, b) => b.isAtivo.CompareTo(a.isAtivo));
+                currentSortState = SortState.Ascending;
+                break;
+        }
+        // Refresh the UI to reflect the sorted order
+        RefreshDishUI();
     }
 
-    public void LoadDishesFromFile()
+    private enum AlphaSortState
     {
-        string path = Application.persistentDataPath + "/dishes.txt";
-
-        if (File.Exists(path))
+        Alpha,
+        NonAlpha
+    }
+    private AlphaSortState currentNameSortState = AlphaSortState.Alpha;
+    public void SortByAlpha()
+    {
+        switch (currentNameSortState)
         {
-            string json = File.ReadAllText(path);
-            SerializableDishList loadedDishes = JsonUtility.FromJson<SerializableDishList>(json);
+            case AlphaSortState.Alpha:
+                dishes.Sort((a, b) => string.Compare(a.nome, b.nome));
+                currentNameSortState = AlphaSortState.NonAlpha;
+                break;
 
-            foreach (Dish dish in loadedDishes.dishes)
-            {
-                // Instantiate the dish UI and set the properties
-                InstantiateDishUI(dish);
-            }
+            case AlphaSortState.NonAlpha:
+                dishes.Sort((a, b) => string.Compare(b.nome, a.nome));
+                currentNameSortState = AlphaSortState.Alpha;
+                break;
+        }
+
+        // Refresh the UI to reflect the sorted order
+        RefreshDishUI();
+    }
+
+    public void SearchDishes()
+    {
+        string query = searchInputField.text.ToLower().Trim(); // Get the search query and convert to lowercase
+
+        // Filter the dishes based on the search query
+        List<Dish> filteredDishes = dishes.Where(dish => dish.nome.ToLower().Contains(query)).ToList();
+
+        // Clear the current UI objects
+        foreach (var dishUI in dishUIObjects)
+        {
+            Destroy(dishUI);
+        }
+        dishUIObjects.Clear();
+
+        // Display only the filtered dishes
+        foreach (var dish in filteredDishes)
+        {
+            InstantiateDishUI(dish);
+        }
+    }
+    private void StartDelayedSearch()
+    {
+        // If there's an ongoing search coroutine, stop it
+        if (searchCoroutine != null)
+        {
+            StopCoroutine(searchCoroutine);
+        }
+
+        // Start a new search coroutine with a delay
+        searchCoroutine = StartCoroutine(DelayedSearch());
+    }
+    private IEnumerator DelayedSearch()
+    {
+        // Wait for 0.5 seconds
+        yield return new WaitForSeconds(0.5f);
+
+        // Execute the search
+        SearchDishes();
+    }
+
+
+
+
+
+    private void RefreshDishUI()
+    {
+        // Clear the current UI objects
+        foreach (var dishUI in dishUIObjects)
+        {
+            Destroy(dishUI);
+        }
+        dishUIObjects.Clear();
+
+        // Re-instantiate the UI objects based on the sorted dishes list
+        foreach (var dish in dishes)
+        {
+            InstantiateDishUI(dish);
         }
     }
 
-    private void InstantiateDishUI(Dish dish)
-    {
-        // Create new dish instance in the UI
-        GameObject newDishUI = Instantiate(dishPrefab, dishesContainer);
-        dishUIObjects.Add(newDishUI);
 
-        // Set properties for the new dish instance
-        TMP_InputField nameField = newDishUI.transform.GetChild(1).GetComponent<TMP_InputField>();
-        TMP_Dropdown categoryDropdown = newDishUI.transform.GetChild(2).GetComponent<TMP_Dropdown>();
-        TMP_InputField halfDoseField = newDishUI.transform.GetChild(3).GetComponent<TMP_InputField>();
-        TMP_InputField fullDoseField = newDishUI.transform.GetChild(4).GetComponent<TMP_InputField>();
-        Toggle soldOutToggle = newDishUI.transform.GetChild(5).GetComponent<Toggle>();
-        Button deleteButton = newDishUI.transform.GetChild(6).GetComponent<Button>();
+    #endregion
 
-        // Populate the UI with the dish data
-        nameField.text = dish.name;
-        categoryDropdown.value = categoryDropdown.options.FindIndex(option => option.text == dish.category); 
-        halfDoseField.text = dish.priceHalfServing.ToString();
-        fullDoseField.text = dish.priceFullServing.ToString();
-        soldOutToggle.isOn = dish.isSoldOut;
-
-        // Disable input for the created dish details (they will only be editable after hitting "Editar")
-        nameField.interactable = false;
-        categoryDropdown.interactable = false;
-        halfDoseField.interactable = false;
-        fullDoseField.interactable = false;
-
-        // Add delete button functionality
-        deleteButton.onClick.AddListener(() =>
-        {
-            DeleteDish(newDishUI);
-        });
-    }
-
-    public void DeleteDish(GameObject dish)
-    {
-        int indexToRemove = dishUIObjects.IndexOf(dish);
-
-        if (indexToRemove >= 0 && indexToRemove < dishes.Count)
-        {
-        dishes.RemoveAt(indexToRemove);
-        SaveDishesToFile();
-        }
-
-        dishUIObjects.Remove(dish);
-        Destroy(dish);
-    }
 }
 
 [System.Serializable]
 public class Dish
 {
-    public string name;
-    public string category;
-    public float priceHalfServing;
-    public float priceFullServing;
-    public bool isSoldOut;
+    public string nome;
+    public string categoria;
+    public float precoMeia;
+    public float precoDose;
+    public bool Esgotado;
+    public bool isAtivo;
+
+    //!Adicionar depois?
+    //public bool novidade = false;
+    //public bool popular = false;
+    //public bool promo = false;
 }
 
 [System.Serializable]
